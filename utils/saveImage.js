86d46e073fb3f5c42c78b8150fd2e63907b00df4
imwaliullah <waliullah0540@gmail.com>
2023-12-28 01:40:24 +0500

    updates

--- utils/saveImage.js
+++ utils/saveImage.js
@@ -22,27 +22,19 @@ async function saveImage(file) {
   const byteData = await file.arrayBuffer();
   const buffer = Buffer.from(byteData);
 
-  // Use the 'public/uploads' directory relative to the project root
-  const uploadsDirectory = join(process.cwd(), "public", "uploads");
-
-  // Ensure the 'public/uploads' directory exists
-  try {
-    await fs.promises.mkdir(uploadsDirectory, { recursive: true });
-  } catch (error) {
-    console.error("Error creating 'public/uploads' directory:", error);
-    throw error;
-  }
+  // Use the 'public' directory relative to the project root
+  const publicDirectory = join(process.cwd(), "public");
 
   // Generate a unique filename based on the current timestamp
   const fileName = `${Date.now()}-${file.name}`;
-  const filePath = join(uploadsDirectory, fileName);
+  const filePath = join(publicDirectory, fileName);
 
   try {
-    // Write the file to the specified path
+    // Write the file to the 'public' directory
     await writeFile(filePath, buffer);
 
     // Construct the URL for the uploaded image
-    const imageUrl = `${baseUrl}/uploads/${fileName}`;
+    const imageUrl = `${baseUrl}/${fileName}`;
     return imageUrl;
   } catch (error) {
     console.error("Error writing file:", error);
